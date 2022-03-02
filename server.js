7e86400d8eaaadb81691a2beb7bbd5ce45029718
maxmcgrath1 <tuc06096@temple.edu>
2022-03-02 11:39:48 -0500

    changed port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@
 require("dotenv").config();
 // pull PORT from .env, give default value of 3000
 // pull MONGODB_URL from .env
-const { PORT = 3000, MONGODB_URL } = process.env;
+const { PORT = 5000, MONGODB_URL } = process.env;
 // import express
 const express = require("express");
 // create application object
