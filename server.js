af9daf80b76ea5762b4a8e9ce0751ff5e1c0e445
maxmcgrath1 <tuc06096@temple.edu>
2022-03-02 10:54:16 -0500

    added people model to server.js

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ const express = require("express");
 const app = express();
 // import mongoose
 const mongoose = require("mongoose");
+// import middlware
+const cors = require("cors");
+const morgan = require("morgan");
 
 ///////////////////////////////
 // DATABASE CONNECTION
@@ -28,11 +31,51 @@ mongoose.connection
   .on("error", (error) => console.log(error));
 
 ///////////////////////////////
+// MODELS
+////////////////////////////////
+const PeopleSchema = new mongoose.Schema({
+  name: String,
+  image: String,
+  title: String,
+});
+
+const People = mongoose.model("People", PeopleSchema);
+
+///////////////////////////////
+// MiddleWare
+////////////////////////////////
+app.use(cors()); // to prevent cors errors, open access to all origins
+app.use(morgan("dev")); // logging
+app.use(express.json()); // parse json bodies
+
+///////////////////////////////
 // ROUTES
 ////////////////////////////////
 // create a test route
 app.get("/", (req, res) => {
   res.send("hello world");
+});
+
+// PEOPLE INDEX ROUTE
+app.get("/people", async (req, res) => {
+    try {
+      // send all people
+        res.json(await People.find({}));
+    } catch (error) {
+      //send error
+        res.status(400).json(error);
+    }
+});
+
+  // PEOPLE CREATE ROUTE
+app.post("/people", async (req, res) => {
+    try {
+      // send all people
+        res.json(await People.create(req.body));
+    } catch (error) {
+      //send error
+        res.status(400).json(error);
+    }
 });
 
 ///////////////////////////////
